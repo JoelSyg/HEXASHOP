00bb3baab88e8879a4f916b6c5490dfd62603c72
Joel <sygullajoel@gmail.com>
2024-11-17 13:28:42 +0100

    chore: rename project and set up initial structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Krist' title`, () => {
+  it(`should have the 'Hexashop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Krist');
+    expect(app.title).toEqual('Hexashop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Krist');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Hexashop');
   });
 });
