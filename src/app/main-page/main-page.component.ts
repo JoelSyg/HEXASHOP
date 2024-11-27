174c2605620a94815e81498187830e3e5414f878
Joel <sygullajoel@gmail.com>
2024-11-27 14:56:50 +0100

    feat: add initial structure for main homepage with hero, latest clothing, and products sections

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
+import { FooterComponent } from "../shared/footer/footer.component";
+import { RouterModule } from '@angular/router';
+import { HeroSectionComponent } from "../hero-section/hero-section.component";
+import { LatestClothingComponent } from '../latest-clothing/latest-clothing.component';
+import { ProductsSectionComponent } from '../products-section/products-section.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule, HeroSectionComponent, LatestClothingComponent, ProductsSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
