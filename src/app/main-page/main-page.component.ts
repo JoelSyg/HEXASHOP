95cf364bf2234f8ce202afb4c9c8643520c9bff4
Joel <sygullajoel@gmail.com>
2024-12-09 09:43:37 +0100

    feat and design: add social-media-section design to main homepage

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../shared/header/header.component";
 import { FooterComponent } from "../shared/footer/footer.component";
 import { RouterModule } from '@angular/router';
+import { SocialMediaSectionComponent } from '../social-media-section/social-media-section.component';
 import { HeroSectionComponent } from "../hero-section/hero-section.component";
 import { LatestClothingComponent } from '../latest-clothing/latest-clothing.component';
 import { ProductsSectionComponent } from '../products-section/products-section.component';
 
+
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterModule, HeroSectionComponent, LatestClothingComponent, ProductsSectionComponent],
+  imports: [HeaderComponent, FooterComponent, RouterModule, HeroSectionComponent, LatestClothingComponent, ProductsSectionComponent, SocialMediaSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
