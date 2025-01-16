9bbb0502d240330669150e99c977db80c32dc3f7
Joel <sygullajoel@gmail.com>
2025-01-16 19:31:46 +0100

    Add dynamic CategoryClothingComponent for men's, women's, and kids' pages; render articles based on route data and integrate with header and routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { CategoryClothingComponent } from './category-clothing/category-clothing.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent},
     { path: 'about-us', component: AboutUsComponent},
+    { path: 'mens-clothing', component: CategoryClothingComponent, data: { category: 'mens' }},
+    { path: 'womens-clothing', component: CategoryClothingComponent, data: { category: 'womens' }},
+    { path: 'kids-clothing', component: CategoryClothingComponent, data: { category: 'kids' }},
 ];
---
+++ src/app/category-clothing/category-clothing.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShopItem } from '../types/shop-item.interface';
+import { ItemService } from '../services/item-service.service';
+import { ProductBoxComponent } from '../product-box/product-box.component';
+import { ActivatedRoute } from '@angular/router';
+import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
+@Component({
+  selector: 'app-category-clothing',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ProductBoxComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  templateUrl: './category-clothing.component.html',
+  styleUrls: ['./category-clothing.component.scss'],
+})
+export class CategoryClothingComponent implements OnInit {
+  @Input() category!: string;
+
+  latestItems = signal<ShopItem[]>([]);
+  casualItems = signal<ShopItem[]>([]);
+  isLoading = signal<boolean>(true);
+
+  constructor(
+    private itemService: ItemService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.category = this.route.snapshot.data['category'];
+    this.loadItems();
+  }
+
+  async loadItems() {
+    try {
+      await this.itemService.loadItems();
+
+      const items = this.itemService
+        .getFilteredItems()
+        .filter((item) => item.category === this.category);
+
+      this.latestItems.set(items.filter((item) => item.new));
+      this.casualItems.set(items.filter((item) => !item.new));
+    } catch (error) {
+      console.error('Fehler beim Laden der Items:', error);
+    } finally {
+      this.isLoading.set(false);
+    }
+  }
+}
