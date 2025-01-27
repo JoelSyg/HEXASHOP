67fa5b34c888cce41e05c373eca528b0173bd0d6
Joel <sygullajoel@gmail.com>
2025-01-27 11:05:32 +0100

    feat: Extend ShopItem interface with size and quantity properties

--- src/app/types/shop-item.interface.ts
+++ src/app/types/shop-item.interface.ts
@@ -1,10 +1,12 @@
 export interface ShopItem {
-    id: string;
-    name: string;
-    category: 'mens' | 'womens' | 'kids';
-    new: boolean;
-    price: number;
-    description?: string;
-    image: string;
-    sizes: Record<string, boolean>;
+  id: string;
+  name: string;
+  category: 'mens' | 'womens' | 'kids';
+  new: boolean;
+  price: number;
+  description?: string;
+  image: string;
+  sizes: Record<string, boolean>;
+  chosenSize?: string;
+  quantity: number;
 }
