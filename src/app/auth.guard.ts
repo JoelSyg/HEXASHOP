435486e3c7a300314b6729c81e2ab555f63f86c0
Joel <sygullajoel@gmail.com>
2025-03-04 23:00:12 +0100

    fix(auth-guard): exit early on non-browser platforms

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,17 +1,22 @@
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserFirebaseService } from './services/user-firebase.service';
+import { isPlatformBrowser } from '@angular/common';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(UserFirebaseService);
   const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
 
-  // Falls User nicht eingeloggt ist, URL speichern und zum Login leiten
+  const isBrowser = isPlatformBrowser(platformId);
+
+  if (!isBrowser) return false;
+
   if (!authService.currentUser()) {
     localStorage.setItem('redirectAfterLogin', state.url);
-    router.navigate(['/auth']); // Leitet zum Login weiter
+    router.navigate(['/auth']);
     return false;
   }
 
-  return true; // Falls eingeloggt, Zugriff erlauben
+  return true;
 };
